ffaed33531ac83c5a8121f22a63ebf93c44df2e4
kjjohnsen <kjjohnsen@users.noreply.github.com>
2023-01-18 23:28:43 -0500

    make changes to each of the three files in lab1

--- lab1/example.py
+++ lab1/example.py
@@ -8,5 +8,9 @@ def find_rhyme(word):
         return "No rhyme found."
 
 word = input("Enter a word: ")
-rhyme = find_rhyme(word)
-print("Rhyme: ", rhyme)
+word = find_rhyme(word)
+print("Rhyme: ", word)
+word = find_rhyme(word)
+print("Rhyme: ", word)
+word = find_rhyme(word)
+print("Rhyme: ", word)
